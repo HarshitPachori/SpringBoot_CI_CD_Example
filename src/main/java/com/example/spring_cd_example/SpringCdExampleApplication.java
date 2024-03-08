760174e1b1bef631ae4dc5fb190d520f935b2728
HarshitPachori <harshitpachori345@gmail.com>
2024-03-08 15:08:11 +0530

    create new spring boot application

---
+++ src/main/java/com/example/spring_cd_example/SpringCdExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_cd_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCdExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCdExampleApplication.class, args);
+	}
+
+}
